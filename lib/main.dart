83f2eee4489ad8f9efe6c09be9020abddf4ae125
Michael Ofosu Darko <mdork76@gmail.com>
2024-09-24 19:11:50 +0000

    main modified

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'src/playlist_details.dart';
 const flutterDevAccountId = 'UCwXdFgeE9KYzlDdR7TG9cMw';
 
 // TODO: Replace with your YouTube API Key
-const youTubeApiKey = 'AIzaSyDc3vOI-GnU8HIeTMmtLnqo3ZTw2adj3uY';
+const youTubeApiKey = 'XXXXXX';
 
 final _router = GoRouter(
   routes: <RouteBase>[
